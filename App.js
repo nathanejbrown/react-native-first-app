49a1de11f6d596af246010b8efbe476dd1080095
Heathcliff <dennis.nathan@gmail.com>
2018-09-03 17:54:33 -0600

    Created a basic TextInput

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default class App extends Component {
+  state = {
+    placeName: ''
+  }
+
+  placeNameChangedHandler = val => {
+    this.setState({
+      placeName: val
+    })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TextInput 
+        style={{width: 300, borderColor: "black", borderWidth: 1}}
+        value={this.state.placeName} 
+        onChangeText={this.placeNameChangedHandler} />
       </View>
     );
   }
