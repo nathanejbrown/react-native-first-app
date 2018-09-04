9c6a7c0f1985d0937e0af2a9e88514fe41be9453
Heathcliff <dennis.nathan@gmail.com>
2018-09-03 18:05:45 -0600

    Added some styles to the TextInput

--- App.js
+++ App.js
@@ -16,7 +16,8 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <TextInput 
-        style={{width: 300, borderColor: "black", borderWidth: 1}}
+        style={{width: 300}}
+        placeholder="An awesome place."
         value={this.state.placeName} 
         onChangeText={this.placeNameChangedHandler} />
       </View>
@@ -27,8 +28,9 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 26,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start'
   },
 });
