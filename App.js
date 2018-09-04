98aee9908d9a3040348c437782eaff42640b4127
Heathcliff <dennis.nathan@gmail.com>
2018-09-03 18:16:58 -0600

    Added a button, more styling.

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default class App extends Component {
   state = {
@@ -15,11 +15,14 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <TextInput 
-        style={{width: 300}}
-        placeholder="An awesome place."
-        value={this.state.placeName} 
-        onChangeText={this.placeNameChangedHandler} />
+        <View style={styles.inputContainer}>
+          <TextInput 
+          style={styles.placeInput}
+          placeholder="An awesome place."
+          value={this.state.placeName} 
+          onChangeText={this.placeNameChangedHandler} />
+          <Button style={styles.placeButton} title="Add" />
+        </View>
       </View>
     );
   }
@@ -33,4 +36,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-start'
   },
+  inputContainer: {
+    // flex: 1,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  placeInput: {
+    width: "70%"
+  },
+  placeButton: {
+    width: "30%"
+  }
 });
