1167cfcaa48b2c885c5ce2921071df8e43bd0ae0
Heathcliff <dennis.nathan@gmail.com>
2018-09-04 12:06:54 -0600

    Added logic to remove a place when it is pressed.

--- src/components/PlaceList/PlaceList.js
+++ src/components/PlaceList/PlaceList.js
@@ -4,7 +4,7 @@ import ListItem from '../ListItem/ListItem';
 
 const placeList = props => {
     const placesOutput = props.places.map((place, i) => (
-        <ListItem key={i} placeName={place} onItemPressed={() => alert('Item pressed: ' + place)} />
+        <ListItem key={i} placeName={place} onItemPressed={() => props.onItemDeleted(i)} />
       ));
 
     return (
