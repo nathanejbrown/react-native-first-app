32a7bff2c979fc72495947f358b30c837a6aa273
Heathcliff <dennis.nathan@gmail.com>
2018-09-05 10:57:53 -0600

    Switched static image to image from web, set up modal to open when a place is selected

--- App.js
+++ App.js
@@ -3,11 +3,12 @@ import { StyleSheet, View } from 'react-native';
 
 import PlaceInput from './src/components/PlaceInput/PlaceInput';
 import PlaceList from './src/components/PlaceList/PlaceList';
-import placeImage from './src/assets/beautiful-place.jpg';
+import PlaceDetail from './src/components/PlaceDetail/PlaceDetail';
 
 export default class App extends Component {
   state = {
-    places: []
+    places: [],
+    selectedPlace: null
   }
 
   placeAddedHandler = placeName => {
@@ -16,17 +17,36 @@ export default class App extends Component {
         places: prevState.places.concat({
           key: Math.random(), 
           name: placeName,
-          image: placeImage
+          image: {
+            uri: 'https://via.placeholder.com/350x350'
+          }
         })
       }
     })
   }
 
-  placeDeletedHandler = key => {
+  placeDeletedHandler = () => {
     this.setState(prevState => {
       return {
         places: prevState.places.filter(place => {
-          return place.key !== key;
+          return place.key !== prevState.selectedPlace.key;
+        }),
+        selectedPlace: null
+      }
+    })
+  }
+
+  modalClosedHandler = () => {
+    this.setState({
+      selectedPlace: null
+    });
+  }
+
+  placeSelectedHandler = key => {
+    this.setState(prevState => {
+      return {
+        selectedPlace: prevState.places.find(place => {
+          return place.key === key;
         })
       }
     })
@@ -35,8 +55,12 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <PlaceDetail 
+        selectedPlace={this.state.selectedPlace} 
+        onItemDeleted={this.placeDeletedHandler} 
+        onModalClosed={this.modalClosedHandler} />
         <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
-        <PlaceList places={this.state.places} onItemDeleted={this.placeDeletedHandler} />
+        <PlaceList places={this.state.places} onItemSelected={this.placeSelectedHandler} />
       </View>
     );
   }
