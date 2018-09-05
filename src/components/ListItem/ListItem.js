55b67cf9a221c360cd7c8ecd403f5647167d75d7
Heathcliff <dennis.nathan@gmail.com>
2018-09-05 10:31:38 -0600

    Added a static image for all places a user adds and styled that to fit each list item.

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
 const listItem = (props) => (
     <TouchableOpacity onPress={props.onItemPressed}>
         <View style={styles.listItem}>
+            <Image source={props.placeImage} style={styles.placeImage} />
             <Text>
                 {props.placeName}
             </Text>
@@ -16,7 +17,14 @@ const styles = StyleSheet.create({
         width: '100%',
         padding: 10,
         backgroundColor: '#eee',
-        margin: 5
+        margin: 5,
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    placeImage: {
+        marginRight: 8,
+        height: 30,
+        width: 30
     }
 });
 
