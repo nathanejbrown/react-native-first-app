981240a84cbfeca9e1b36b891de49f526e7a68c9
Heathcliff <dennis.nathan@gmail.com>
2018-09-15 15:06:12 -0600

    Started rebuilding app using react-native-navigation.

--- ios/build/Build/Intermediates.noindex/myfirstapp.build/Debug-iphonesimulator/myfirstapp.build/Objects-normal/x86_64/AppDelegate.d
+++ ios/build/Build/Intermediates.noindex/myfirstapp.build/Debug-iphonesimulator/myfirstapp.build/Objects-normal/x86_64/AppDelegate.d
@@ -2,11 +2,12 @@ dependencies: \
   /Users/Nathan/udemy/React-Native-Course/my-first-app/ios/myfirstapp/AppDelegate.m \
   /Users/Nathan/udemy/React-Native-Course/my-first-app/ios/myfirstapp/AppDelegate.h \
   /Users/Nathan/udemy/React-Native-Course/my-first-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBundleURLProvider.h \
+  ../node_modules/react-native-navigation/ios/RCCManager.h \
+  /Users/Nathan/udemy/React-Native-Course/my-first-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeModule.h \
+  /Users/Nathan/udemy/React-Native-Course/my-first-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
   /Users/Nathan/udemy/React-Native-Course/my-first-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTRootView.h \
   /Users/Nathan/udemy/React-Native-Course/my-first-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridge.h \
   /Users/Nathan/udemy/React-Native-Course/my-first-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeDelegate.h \
   /Users/Nathan/udemy/React-Native-Course/my-first-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTJavaScriptLoader.h \
-  /Users/Nathan/udemy/React-Native-Course/my-first-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
-  /Users/Nathan/udemy/React-Native-Course/my-first-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeModule.h \
   /Users/Nathan/udemy/React-Native-Course/my-first-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTFrameUpdate.h \
   /Users/Nathan/udemy/React-Native-Course/my-first-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTInvalidating.h
