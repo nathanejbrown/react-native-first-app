2c83dc9518da02b012c10be0f0fbcf2fc22144e6
Heathcliff <dennis.nathan@gmail.com>
2018-09-17 12:50:10 -0600

    Updated app to use Redux.

--- src/screens/FindPlace/FindPlace.js
+++ src/screens/FindPlace/FindPlace.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+
+import PlaceList from '../../components/PlaceList/PlaceList';
 
 class FindPlaceScreen extends Component {
   render () {
     return (
       <View>
+        <PlaceList places={this.props.places} />
         <Text>On FindPlaceScreen</Text>
       </View>
     )
   }
 }
 
-export default FindPlaceScreen;
+const mapStateToProps = state => {
+  return {
+    places: state.places.places
+  }
+}
+
+export default connect(mapStateToProps)(FindPlaceScreen);
