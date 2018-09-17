6caebd140c04770bd5210ec0cb3fc4188fcfa885
Heathcliff <dennis.nathan@gmail.com>
2018-09-17 15:00:32 -0600

    Got the place detail functionality up and running using page navigation.

--- App.js
+++ App.js
@@ -4,7 +4,9 @@ import { Provider } from 'react-redux';
 import AuthScreen from './src/screens/Auth/Auth';
 import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
+import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
 import configureStore from './src/store/configureStore';
+
 
 const store = configureStore();
 
@@ -12,6 +14,7 @@ const store = configureStore();
 Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen, store, Provider);
 Navigation.registerComponent('awesome-places.SharePlaceScreen', () => SharePlaceScreen, store, Provider);
 Navigation.registerComponent('awesome-places.FindPlaceScreen', () => FindPlaceScreen, store, Provider);
+Navigation.registerComponent('awesome-places.PlaceDetailScreen', () => PlaceDetailScreen);
 
 // Start an app
 Navigation.startSingleScreenApp({
