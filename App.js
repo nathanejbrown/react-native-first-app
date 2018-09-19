0b7a25b5e9d3298e261940a36aff04c6e47d5449
Heathcliff <dennis.nathan@gmail.com>
2018-09-19 13:06:52 -0600

    Added sidedrawer, set up a button to toggle it.

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import SharePlaceScreen from './src/screens/SharePlace/SharePlace';
 import FindPlaceScreen from './src/screens/FindPlace/FindPlace';
 import PlaceDetailScreen from './src/screens/PlaceDetail/PlaceDetail';
 import configureStore from './src/store/configureStore';
+import SideDrawer from './src/screens/SideDrawer/SideDrawer';
 
 
 const store = configureStore();
@@ -15,6 +16,7 @@ Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen, stor
 Navigation.registerComponent('awesome-places.SharePlaceScreen', () => SharePlaceScreen, store, Provider);
 Navigation.registerComponent('awesome-places.FindPlaceScreen', () => FindPlaceScreen, store, Provider);
 Navigation.registerComponent('awesome-places.PlaceDetailScreen', () => PlaceDetailScreen, store, Provider);
+Navigation.registerComponent('awesome-places.SideDrawer', () => SideDrawer)
 
 // Start an app
 Navigation.startSingleScreenApp({
