24e0de9a81b6a6e00a755a54795cc62ea79fa37e
Heathcliff <dennis.nathan@gmail.com>
2018-09-19 12:32:30 -0600

    Added forward and backward navigation with push and pop methods.

--- src/screens/PlaceDetail/PlaceDetail.js
+++ src/screens/PlaceDetail/PlaceDetail.js
@@ -1,24 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Image, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { connect } from 'react-redux';
+import { deletePlace } from '../../store/actions/index';
+
 import Icon from 'react-native-vector-icons/Ionicons';
 
-const placeDetail = props => {
+class PlaceDetail extends Component {
 
-    return (
-        <View style={styles.container}>
-            <View>
-                <Image source={props.selectedPlace.image} style={styles.placeImage} />
-                <Text style={styles.placeName}>{props.selectedPlace.name}</Text>
+    placeDeletedHandler = () => {
+        this.props.onDeletePlace(this.props.selectedPlace.key);
+        this.props.navigator.pop();
+    }
+
+    render () {
+        return (
+            <View style={styles.container}>
+                <View>
+                    <Image source={this.props.selectedPlace.image} style={styles.placeImage} />
+                    <Text style={styles.placeName}>{this.props.selectedPlace.name}</Text>
+                </View>
+                <View>
+                    <TouchableOpacity onPress={this.placeDeletedHandler}>
+                        <View style={styles.deleteButton}>
+                            <Icon size={30} name="ios-trash" color="red" />
+                        </View>
+                    </TouchableOpacity>
+                </View>
             </View>
-            <View>
-                <TouchableOpacity onPress={props.onItemDeleted}>
-                    <View style={styles.deleteButton}>
-                        <Icon size={30} name="ios-trash" color="red" />
-                    </View>
-                </TouchableOpacity>
-            </View>
-        </View>
-    );
+        );
+    }
+
 }
 
 const styles = StyleSheet.create({
@@ -39,4 +50,10 @@ const styles = StyleSheet.create({
     }
 })
 
-export default placeDetail;
+const mapDispatchToProps = dispatch => {
+    return {
+        onDeletePlace: (key) => dispatch(deletePlace(key))
+    }
+}
+
+export default connect(null, mapDispatchToProps)(PlaceDetail);
