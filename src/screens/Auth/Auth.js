a02f0ca42578e866dc8df144b71a38e27ae2459d
Heathcliff <dennis.nathan@gmail.com>
2018-09-21 12:04:06 -0600

    Created MainText component.

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -4,6 +4,7 @@ import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 import startMainTabs from '../MainTabs/startMainTabs';
 import DefaultInput from '../../components/UI/DefaultInput/DefaultInput';
 import HeadingText from '../../components/UI/HeadingText/HeadingText';
+import MainText from '../../components/UI/MainText/MainText';
 
 class AuthScreen extends Component {
 
@@ -14,7 +15,9 @@ class AuthScreen extends Component {
   render () {
       return (
           <View style={styles.container}>
+            <MainText>
               <HeadingText>Please Log In</HeadingText>
+            </MainText>
               <Button title="Switch to Login" />
               <View style={styles.inputContainer}>
                 <DefaultInput placeholder="Your Email Address" style={styles.input} />
